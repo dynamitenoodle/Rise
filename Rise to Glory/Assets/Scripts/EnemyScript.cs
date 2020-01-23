947c402ec2ae161d7a710ac5fc34190a1ba1d428
Coehl Gleckner (RIT Student) <crg5646@ad.rit.edu>
2020-01-22 22:04:44 -0500

    Basic player and enemy
    I shoulda done this in another branch

---
+++ Rise to Glory/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public float maxSpeed = .1f;
+    public float speed = .02f;
+    public float friction = .9f;
+    public float detectionDistance = 10f;
+    public float attackRange = 1f;
+    Vector3 velocity;
+    Vector3 direction;
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        velocity = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float playerDis = (player.transform.position - transform.position).magnitude;
+
+        // Where is PLAYER
+        if (playerDis < detectionDistance && !AttackCheck(playerDis))
+            direction = (player.transform.position - transform.position).normalized;
+
+        // Slowdown if nothing
+        if (direction == Vector3.zero)
+            velocity *= friction;
+
+        velocity += (direction * speed);
+
+        velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+
+        // Carry out the math
+        transform.position += velocity;
+        direction = Vector3.zero;
+    }
+
+    bool AttackCheck(float playerDis)
+    {
+        if (attackRange > playerDis)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
