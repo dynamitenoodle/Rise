bbc36e38fbae9764c7af05c3f752165267502259
Coehl Gleckner (RIT Student) <crg5646@ad.rit.edu>
2020-01-23 13:53:17 -0500

    Attack is starting to come together
    -Issue, the collisions aren't working (big oof, but can fix)

--- Rise to Glory/Assets/Scripts/EnemyScript.cs
+++ Rise to Glory/Assets/Scripts/EnemyScript.cs
@@ -14,8 +14,15 @@ public class EnemyScript : MonoBehaviour
     Vector3 direction;
     GameObject player;
 
+    // attack stuff
     bool attacking;
     Vector3 attackDir;
+    public float attackTimerMax = 1.5f;
+    public float attackDelay = .5f;
+    public float attackSpacing = 1.5f;
+    float attackTimer;
+    public GameObject attackPrefab;
+    GameObject attack;
 
     // Start is called before the first frame update
     void Start()
@@ -41,17 +48,55 @@ public class EnemyScript : MonoBehaviour
 
         velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
 
+        // Attacking stuff
+        if (attacking)
+        {
+            attackTimer += Time.deltaTime;
+
+            // Create the attack when it is time
+            if (attackTimer > attackDelay && attack == null)
+            {
+                attack = Instantiate(attackPrefab);
+                attack.transform.position = transform.position + (attackDir * attackSpacing);
+                attack.transform.right = attackDir;
+                velocity += attackDir * (speed*2.5f);
+
+                // Checking the bounds
+                Bounds playerBounds = player.GetComponent<BoxCollider2D>().bounds;
+                playerBounds.extents += Vector3.forward * Mathf.Infinity; // scale the bounds to infinity on z
+
+                Bounds attackBounds = attack.GetComponent<BoxCollider2D>().bounds;
+                attackBounds.extents += Vector3.forward * Mathf.Infinity;
+
+                if (attackBounds.Intersects(playerBounds))
+                    player.GetComponent<PlayerScript>().GetHit();
+            }
+
+            // Reset the attack timer
+            if (attackTimer >= attackTimerMax)
+            {
+                Destroy(attack);
+                attackTimer = 0;
+                attacking = false;
+            }
+        }
+
         // Carry out the math
         transform.position += velocity;
         direction = Vector3.zero;
     }
 
+    // The check for attacking
     bool AttackCheck(float playerDis)
     {
-        if (attackRange > playerDis)
+        if (attacking)
+            return true;
+
+        // IS IT TIME TO ATTACK
+        else if (attackRange > playerDis)
         {
             attacking = true;
-            attackDir = direction;
+            attackDir = (player.transform.position - transform.position).normalized;
             return true;
         }
 
