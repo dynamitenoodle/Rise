905f5d21334aafcd5ae30288ef589e5de2d5950e
Justin <jgfoxbox@gmail.com>
2020-01-24 20:47:24 -0500

    Added start of level generation work

---
+++ Rise to Glory/Assets/Scripts/Utility/Helper.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helper
+{
+    
+    public int getLarger(int a, int b)
+    {
+        return (a > b) ? a : b;
+    }
+
+    public float getLarger(float a, float b)
+    {
+        return (a > b) ? a : b;
+    }
+}
