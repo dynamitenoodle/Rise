e7ad7f978207f22096f9932d4faf28831b1b445f
Justin's Stupid Computer <jgfoxbox@gmail.com>
2020-01-26 15:05:01 -0500

    Fix

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
+    // attributes
     public float maxSpeed = .1f;
     public float speed = .02f;
     public float friction = .9f;
@@ -13,11 +14,26 @@ public class EnemyScript : MonoBehaviour
     Vector3 direction;
     GameObject player;
 
+    // attack stuff
+    bool attacking;
+    Vector3 attackDir;
+    public float attackTimerMax = 1.5f;
+    public float attackDelay = .5f;
+    public float attackSpacing = 1.5f;
+    float attackTimer;
+    public GameObject attackPrefab;
+    GameObject attack;
+
+    // health
+    public float healthMax = 2;
+    float health;
+
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         velocity = Vector3.zero;
+        health = healthMax;
     }
 
     // Update is called once per frame
@@ -26,7 +42,7 @@ public class EnemyScript : MonoBehaviour
         float playerDis = (player.transform.position - transform.position).magnitude;
 
         // Where is PLAYER
-        if (playerDis < detectionDistance && !AttackCheck(playerDis))
+        if (playerDis < detectionDistance && !AttackCheck(playerDis) && !attacking)
             direction = (player.transform.position - transform.position).normalized;
 
         // Slowdown if nothing
@@ -37,15 +53,49 @@ public class EnemyScript : MonoBehaviour
 
         velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
 
+        // Attacking stuff
+        if (attacking)
+        {
+            attackTimer += Time.deltaTime;
+
+            // Create the attack when it is time
+            if (attackTimer > attackDelay && attack == null)
+            {
+                attack = Instantiate(attackPrefab);
+                attack.transform.position = transform.position + (attackDir * attackSpacing);
+                attack.transform.right = attackDir;
+                velocity += attackDir * (speed * 2.5f);
+
+                // Checking if player gets hit
+                if (player.GetComponent<Renderer>().bounds.Intersects(attack.GetComponent<Renderer>().bounds))
+                    player.GetComponent<PlayerScript>().GetHit();
+            }
+
+            // Reset the attack timer
+            if (attackTimer >= attackTimerMax)
+            {
+                Destroy(attack);
+                attackTimer = 0;
+                attacking = false;
+            }
+        }
+
         // Carry out the math
         transform.position += velocity;
         direction = Vector3.zero;
     }
 
+    // The check for attacking
     bool AttackCheck(float playerDis)
     {
-        if (attackRange > playerDis)
+        if (attacking)
+            return true;
+
+        // IS IT TIME TO ATTACK
+        else if (attackRange > playerDis)
         {
+            attacking = true;
+            attackDir = (player.transform.position - transform.position).normalized;
             return true;
         }
 
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    // attributes
     public float maxSpeed = .1f;
     public float speed = .02f;
     public float friction = .9f;
@@ -11,11 +12,19 @@ public class PlayerScript : MonoBehaviour
     Vector3 velocity;
     Vector3 direction;
 
+    // health stuffs
+    public float healthMax;
+    float health;
+    bool invul;
+    public float hitTimerMax;
+    float hitTimer;
+
     // Start is called before the first frame update
     void Start()
     {
         velocity = Vector3.zero;
-
+        health = healthMax;
+        invul = false;
     }
 
     // Update is called once per frame
@@ -46,5 +55,41 @@ public class PlayerScript : MonoBehaviour
         // Carry out the math
         transform.position += velocity;
         direction = Vector3.zero;
+
+        if (invul)
+        {
+            hitTimer += Time.deltaTime;
+
+            //Flicker effect
+            Color tempColor = GetComponent<SpriteRenderer>().color;
+            if ((hitTimer * 100) % 20 > 10)
+                tempColor.a = .6f;
+            else
+                tempColor.a = 1f;
+
+            GetComponent<SpriteRenderer>().color = tempColor;
+
+            if (hitTimer > hitTimerMax)
+            {
+                hitTimer = 0;
+                invul = false;
+                tempColor.a = 1f;
+                GetComponent<SpriteRenderer>().color = tempColor;
+            }
+        }
+    }
+
+    // Called by other scripts to hit the player
+    public void GetHit()
+    {
+        if (!invul)
+        {
+            invul = true;
+            Debug.Log("Player was hit!");
+            health--;
+
+            if (health < 0)
+                Destroy(gameObject);
+        }
     }
 }
