7cf97219336a372a2042d31b814a6ef9fdfd2cd6
Coehl Gleckner (RIT Student) <crg5646@ad.rit.edu>
2020-01-27 19:28:29 -0500

    Working on Collisions

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -19,12 +19,21 @@ public class PlayerScript : MonoBehaviour
     public float hitTimerMax;
     float hitTimer;
 
+    // Wall collisions
+    public List<GameObject> walls;
+
     // Start is called before the first frame update
     void Start()
     {
         velocity = Vector3.zero;
         health = healthMax;
         invul = false;
+
+        foreach (GameObject wall in GameObject.FindGameObjectsWithTag("Wall"))
+        {
+            walls.Add(wall);
+        }
+
     }
 
     // Update is called once per frame
@@ -44,6 +53,8 @@ public class PlayerScript : MonoBehaviour
         if (direction == Vector3.zero)
             velocity *= friction;
 
+        WallCheck();
+
         velocity += (direction * speed);
 
         // Sprinting
@@ -58,25 +69,7 @@ public class PlayerScript : MonoBehaviour
 
         if (invul)
         {
-            hitTimer += Time.deltaTime;
-
-            //Flicker effect
-            Color tempColor = GetComponent<SpriteRenderer>().color;
-            
-            if ((hitTimer * 100) % 20 > 10)
-                tempColor.a = .6f;
-            else
-                tempColor.a = 1f;
-
-            GetComponent<SpriteRenderer>().color = tempColor;
-
-            if (hitTimer > hitTimerMax)
-            {
-                hitTimer = 0;
-                invul = false;
-                tempColor.a = 1f;
-                GetComponent<SpriteRenderer>().color = tempColor;
-            }
+            Flicker();
         }
     }
 
@@ -91,6 +84,108 @@ public class PlayerScript : MonoBehaviour
 
             if (health < 0)
                 Destroy(gameObject);
+        }
+    }
+
+    // Flickering if hit
+    public void Flicker()
+    {
+        hitTimer += Time.deltaTime;
+
+        //Flicker effect
+        Color tempColor = GetComponent<SpriteRenderer>().color;
+
+        if ((hitTimer * 100) % 20 > 10)
+            tempColor.a = .6f;
+        else
+            tempColor.a = 1f;
+
+        GetComponent<SpriteRenderer>().color = tempColor;
+
+        if (hitTimer > hitTimerMax)
+        {
+            hitTimer = 0;
+            invul = false;
+            tempColor.a = 1f;
+            GetComponent<SpriteRenderer>().color = tempColor;
+        }
+    }
+
+    void WallCheck()
+    {
+        List<GameObject> closeWalls = new List<GameObject>();
+        closeWalls.Add(walls[0]);
+        float closeDis = Vector3.Distance(closeWalls[0].transform.position, transform.position);
+        foreach (GameObject wall in walls)
+        {
+            float newDis = Vector3.Distance(wall.transform.position, transform.position);
+            if (newDis < closeDis)
+            {
+                closeWalls = new List<GameObject>();
+                closeWalls.Add(wall);
+                closeDis = newDis;
+            }
+            else if (newDis == closeDis && wall != walls[0])
+            {
+                closeWalls.Add(wall);
+            }
+        }
+
+        foreach (GameObject wall in closeWalls)
+        {
+            if (GetComponent<Renderer>().bounds.Intersects(wall.GetComponent<Renderer>().bounds))
+            {
+                // Make an easier variable
+                Vector3 wallPos = wall.transform.position;
+
+                // Calculate the bounding boxes of the player and the wall
+                float playerRight = transform.position.x + GetComponent<Renderer>().bounds.extents.x;
+                float playerLeft = transform.position.x - GetComponent<Renderer>().bounds.extents.x;
+                float playerTop = transform.position.y + GetComponent<Renderer>().bounds.extents.y;
+                float playerBot = transform.position.y - GetComponent<Renderer>().bounds.extents.y;
+
+                float wallRight = wallPos.x + wall.GetComponent<Renderer>().bounds.extents.x;
+                float wallLeft = wallPos.x - wall.GetComponent<Renderer>().bounds.extents.x;
+                float wallTop = wallPos.y + wall.GetComponent<Renderer>().bounds.extents.y;
+                float wallBot = wallPos.y - wall.GetComponent<Renderer>().bounds.extents.y;
+
+                // Set a distance to check walls (the number here works well with 1x1 boxes)
+                float dis = .08f;
+
+                // Checking which side to stop the player
+                // Right
+                if (Mathf.Abs(playerRight - wallLeft) < dis)
+                {
+                    if (direction.x > 0)
+                        direction.x = 0;
+                    if (velocity.x > 0)
+                        velocity.x = 0;
+                }
+                // Left
+                if (Mathf.Abs(playerLeft - wallRight) < dis)
+                {
+                    if (direction.x < 0)
+                        direction.x = 0;
+                    if (velocity.x < 0)
+                        velocity.x = 0;
+                }
+                // Up
+                if (Mathf.Abs(playerTop - wallBot) < dis)
+                {
+                    if (direction.y > 0)
+                        direction.y = 0;
+                    if (velocity.y > 0)
+                        velocity.y = 0;
+                }
+                // Down
+                if (Mathf.Abs(playerBot - wallTop) < dis)
+                {
+                    if (direction.y < 0)
+                        direction.y = 0;
+                    if (velocity.y < 0)
+                        velocity.y = 0;
+                }
+            }
         }
     }
 }
