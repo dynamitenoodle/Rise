60b1aa695152b92462a95b175204aefc33a904ac
Justin's Stupid Computer <jgfoxbox@gmail.com>
2020-02-04 16:54:22 -0500

    Redoing level gen to work with new implementations - currently got everything working as it was in previous commit, but using less variables now

--- Assets/Scripts/Level - Floors/LevelGeneration.cs
+++ Assets/Scripts/Level - Floors/LevelGeneration.cs
@@ -22,8 +22,7 @@ public class LevelGeneration : MonoBehaviour
         public int type;
         public Vector2 location;
         public int roomSize;
-        public List<Vector2> doorsLoc;
-        public List<DoorDescriber> doorsDesc;
+        public List<DoorDescriber> doors;
         public List<int> connections;
     }
 
@@ -73,44 +72,37 @@ public class LevelGeneration : MonoBehaviour
             {
                 //pick room that already exists
                 int roomPick = Random.Range(0, roomSpawns.Count);
-                int doorPick = Random.Range(0, roomSpawns[roomPick].doorsLoc.Count);
+                int doorPick = Random.Range(0, roomSpawns[roomPick].doors.Count);
 
                 Vector2 doorPositionAdjust = GetDoorAdjust(roomSpawns[roomPick], doorPick);
 
                 //find door of new room
                 roomSpawn = GenerateRoom((int)roomSpawns[roomPick].location.x, (int)roomSpawns[roomPick].location.y);
                 Vector2 roomMove = Vector2.zero;
-                for (int j = 0; j < roomSpawn.doorsLoc.Count; j++)
+                for (int j = 0; j < roomSpawn.doors.Count; j++)
                 {
                     Vector2 doorAdjust = GetDoorAdjust(roomSpawn, j);
                     if (IsDoorInverse(doorPositionAdjust, doorAdjust))
                     {
                         Vector2 doorLoc = new Vector2(
-                            roomSpawn.location.x + roomSpawn.doorsLoc[j].x,
-                            roomSpawn.location.y + roomSpawn.doorsLoc[j].y);
+                            roomSpawn.location.x + roomSpawn.doors[j].location.x,
+                            roomSpawn.location.y + roomSpawn.doors[j].location.y);
                         Vector2 matchLoc = new Vector2(
-                            roomSpawns[roomPick].location.x + roomSpawns[roomPick].doorsLoc[doorPick].x + doorPositionAdjust.x,
-                            roomSpawns[roomPick].location.y + roomSpawns[roomPick].doorsLoc[doorPick].y + doorPositionAdjust.y);
+                            roomSpawns[roomPick].location.x + roomSpawns[roomPick].doors[doorPick].location.x + doorPositionAdjust.x,
+                            roomSpawns[roomPick].location.y + roomSpawns[roomPick].doors[doorPick].location.y + doorPositionAdjust.y);
                         Debug.Log($"matchLoc: {matchLoc}, doorLoc: {doorLoc}");
                         roomMove = matchLoc - doorLoc;
 
                         //remove used doors
-                        roomSpawns[roomPick].doorsLoc.RemoveAt(doorPick);
-                        roomSpawns[roomPick].doorsDesc.RemoveAt(doorPick);
+                        roomSpawns[roomPick].doors.RemoveAt(doorPick);
 
-                        roomSpawn.doorsLoc.RemoveAt(j);
-                        roomSpawn.doorsDesc.RemoveAt(j);
+                        roomSpawn.doors.RemoveAt(j);
 
                         break;
                     }
                 }
 
                 roomSpawn.location += roomMove;
-                //update door locations
-                for (int j = 0; j < roomSpawn.doorsLoc.Count; j++)
-                {
-                    roomSpawn.doorsLoc[j] += roomMove;
-                }
 
                 //Debug.Log($"New room spawned moving it: {roomMove} ");
                 Debug.Log($"Spawning room at loc: {roomSpawn.location} of type [{roomSpawn.type}] from room {roomPick}");
@@ -146,7 +138,7 @@ public class LevelGeneration : MonoBehaviour
 
     private Vector2 GetDoorAdjust(RoomSpawn room, int door)
     {
-        DoorDescriber doorDescriber = room.doorsDesc[door];
+        DoorDescriber doorDescriber = room.doors[door];
         switch (doorDescriber.doorLocation)
         {
             case DoorDescriber.DoorLocation.North:
@@ -177,17 +169,16 @@ public class LevelGeneration : MonoBehaviour
         roomSpawn.connections = new List<int>();
 
         //add each doorway
-        List<Vector2> doors = new List<Vector2>();
-        List<DoorDescriber> doorsDesc = new List<DoorDescriber>();
+        List<DoorDescriber> doors = new List<DoorDescriber>();
         for (int i = 0; i < describer.doorways.Count; i++)
         {
-            doors.Add(new Vector2(
-                describer.doorways[i].transform.position.x + x, 
-                describer.doorways[i].transform.position.y + y));
-            doorsDesc.Add(describer.doorways[i].GetComponent<DoorDescriber>());
+            doors.Add(describer.doorways[i].GetComponent<DoorDescriber>());
+            doors[i].location = new Vector2(
+                describer.doorways[i].transform.position.x, 
+                describer.doorways[i].transform.position.y);
+            Debug.Log($"Door for room [{i}] spawning at position: {doors[i].location}");
         }
-        roomSpawn.doorsLoc = doors;
-        roomSpawn.doorsDesc = doorsDesc;
+        roomSpawn.doors = doors;
         return roomSpawn;
     }
 
@@ -198,141 +189,13 @@ public class LevelGeneration : MonoBehaviour
             GameObject room = Instantiate(rooms[roomSpawn.type], mapTransform);
             room.transform.position = roomSpawn.location;
 
-            for (int i = 0; i < roomSpawn.doorsLoc.Count; i++)
+            for (int i = 0; i < roomSpawn.doors.Count; i++)
             {
-                GameObject door = Instantiate(tester);
-                door.transform.position = roomSpawn.doorsLoc[i];
+                GameObject doorTester = Instantiate(tester);
+                doorTester.transform.position = roomSpawn.location + roomSpawn.doors[i].location;
+                Debug.Log($"Spawning door tester obj @ location: {doorTester.transform.position} from location {roomSpawn.doors[i].location}");
             }
         }
         Debug.Log("FINISH");
     }
-    /*
-    private List<Hall> GenerateHalls(List<RoomSpawn> roomSpawns)
-    {
-        List<Hall> halls = new List<Hall>();
-        for (int i = 0; i < roomSpawns.Count; i++)
-        {
-            int closestRoom = FindClosestRoom(roomSpawns, i);
-            Debug.Log($"Found room {closestRoom} as closest room to {i}");
-            int closestDoorStartRoom = FindClosestDoor(roomSpawns[i], roomSpawns[closestRoom]);
-            int closestDoorCloseRoom = FindClosestDoor(roomSpawns[closestRoom], roomSpawns[i]);
-
-            List<Hall> newHalls = CreateHalls(roomSpawns[i].doors[closestDoorStartRoom], roomSpawns[closestRoom].doors[closestDoorCloseRoom]);
-            halls.AddRange(newHalls);
-
-            roomSpawns[i].doors.RemoveAt(closestDoorStartRoom);
-            roomSpawns[closestRoom].doors.RemoveAt(closestDoorCloseRoom);
-            roomSpawns[i].connections.Add(closestRoom);
-            roomSpawns[closestRoom].connections.Add(i);
-        }
-
-        return halls;
-    }
-
-    private List<Hall> CreateHalls(Vector2 start, Vector2 end)
-    {
-        List<Hall> halls = new List<Hall>();
-        bool finish = false;
-
-        Vector2 cur = start;
-
-        do
-        {
-
-            if (cur.x != end.x)
-            {
-                if (cur.x < end.x)
-                {
-                    cur.x += 1;
-                }
-                else
-                {
-                    cur.x -= 1;
-                }
-            }
-            else if (cur.y != end.y)
-            {
-                if (cur.y < end.y)
-                {
-                    cur.y += 1;
-                }
-                else
-                {
-                    cur.y -= 1;
-                }
-            }
-
-            Hall newHall = new Hall();
-            newHall.type = 0;
-            newHall.location = cur;
-            halls.Add(newHall);
-
-            if (cur.Equals(end))
-            {
-                finish = true;
-            }
-
-        } while (!finish);
-
-        return halls;
-    }
-
-    private bool IsConnected(List<int> connections, int roomNum)
-    {
-        for (int i = 0; i < connections.Count; i++)
-        {
-            if (connections[i] == roomNum)
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private int FindClosestRoom(List<RoomSpawn> roomSpawns, int roomNum)
-    {
-        //find nearest room
-        int nearestRoom = -1;
-        float closestDistance = -1;
-        for (int j = 0; j < roomSpawns.Count; j++)
-        {
-            if ((roomSpawns[j].location - roomSpawns[roomNum].location).magnitude < closestDistance || nearestRoom == -1)
-            {
-                if (j != roomNum && !IsConnected(roomSpawns[roomNum].connections, roomNum))
-                { 
-                    nearestRoom = j;
-                    closestDistance = (roomSpawns[j].location - roomSpawns[roomNum].location).magnitude;
-                }
-            }
-        }
-        return nearestRoom;
-    }
-
-    private int FindClosestDoor(RoomSpawn doorRoom, RoomSpawn toRoom)
-    {
-        int closest = -1;
-        float closestDistance = -1;
-        for (int i = 0; i < doorRoom.doors.Count; i++)
-        {
-            if ((doorRoom.doors[i] - toRoom.location).magnitude < closestDistance || closest == -1)
-            {
-                closest = i;
-                closestDistance = (doorRoom.doors[i] - toRoom.location).magnitude;
-            }
-        }
-
-        return closest;
-    }
-
-
-    private void InstantiateHalls(List<Hall> halls)
-    {
-        foreach (Hall hall in halls)
-        {
-            GameObject room = Instantiate(hallways[hall.type], mapTransform);
-            room.transform.position = hall.location;
-        }
-        Debug.Log("FINISH");
-    }
-    */
 }
